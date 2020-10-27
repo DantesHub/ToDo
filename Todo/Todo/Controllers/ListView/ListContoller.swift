2e312d521c93b7e9e6b54a63cfac0778c0567914
Dante <motiv8lab@gmail.com>
2020-10-26 20:44:53 -0400

    added textfield above keyboard

--- Todo/Todo/Controllers/ListView/ListContoller.swift
+++ Todo/Todo/Controllers/ListView/ListContoller.swift
@@ -16,7 +16,7 @@ protocol ReloadDelegate {
 }
 
 extension ListController: KeyboardToolbarDelegate {
-   func keyboardToolbar(button: UIBarButtonItem, type: KeyboardToolbarButton, isInputAccessoryViewOf textField: UITextField) {
+    func keyboardToolbar(button: UIBarButtonItem, type: KeyboardToolbarButton, isInputAccessoryViewOf textField: UITextField) {
         switch type {
         case .done:
             addTaskField.resignFirstResponder()
@@ -30,9 +30,12 @@ extension ListController: KeyboardToolbarDelegate {
             print("dingo")
         case .favorite:
             print("way up high")
-    }
+        }
     }
 }
+var keyboard = false
+var lastKeyboardHeight: CGFloat = 0
+
 class ListController: UIViewController {
     //MARK: - instance variables
     var reloadDelegate: ReloadDelegate?
@@ -65,6 +68,7 @@ class ListController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
     //MARK: - helper variables
@@ -72,6 +76,10 @@ class ListController: UIViewController {
         configureNavBar()
         createTableHeader()
         createTableView()
+        let center: NotificationCenter = NotificationCenter.default
+        center.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        center.addObserver(self, selector: #selector(keyboardWillChangeFrame(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         plusTaskView = UIImageView(frame: CGRect(x: self.view.frame.width - 100, y: self.view.frame.height - 130 , width: 60, height: 60))
         view.addSubview(plusTaskView)
         let padding:CGFloat = 10
@@ -87,28 +95,25 @@ class ListController: UIViewController {
         plusTaskView.addGestureRecognizer(addTaskRecognizer)
         plusTaskView.isUserInteractionEnabled = true
         plusTaskView.dropShadow()
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedOutside))
+        self.view.addGestureRecognizer(tapRecognizer)
+        addTaskField.isHidden = false
+        addTaskField.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: 40)
+        addTaskField.addKeyboardToolBar(leftButtons: [.addToList, .priority, .dueDate, .reminder, .favorite], rightButtons: [.done], toolBarDelegate: self)
+        view.addSubview(addTaskField)
+        addTaskField.backgroundColor = .black
+//        addTaskField.delegate = self
+        addTaskField.borderStyle = .roundedRect
     }
     
     @objc func tappedAddTask() {
-        print("tappedAddTask")
-        let center: NotificationCenter = NotificationCenter.default
-          center.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-          center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        view.addSubview(addTaskField)
         addTaskField.becomeFirstResponder()
-
-        }
-
-        private func createTextField(frame: CGRect, leftButtons: [KeyboardToolbarButton] = [], rightButtons: [KeyboardToolbarButton] = []) {
-            print("for all time")
-            addTaskField.removeFromSuperview()
-            addTaskField = UITextField(frame: frame)
-            view.addSubview(addTaskField)
-            addTaskField.becomeFirstResponder()
-            addTaskField.delegate = self
-            addTaskField.borderStyle = .roundedRect
-            addTaskField.addKeyboardToolBar(leftButtons: leftButtons, rightButtons: rightButtons, toolBarDelegate: self)
-        }
+    }
+    
+    @objc func tappedOutside() {
+        self.view.endEditing(true)
+    }
+    
     
     func createTableHeader() {
         let headerView: UIView = UIView.init(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 100))
@@ -133,7 +138,6 @@ class ListController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "list")
         tableView.dataSource = self
         tableView.separatorStyle = .none
-   
         view.addSubview(tableView)
         tableView.leadingToSuperview()
         tableView.trailingToSuperview()
@@ -144,10 +148,9 @@ class ListController: UIViewController {
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
         swipeDown.direction = .down
         self.tableView.addGestureRecognizer(swipeDown)
-        
         let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
-          swipeUp.direction = .up
-          self.tableView.addGestureRecognizer(swipeUp)
+        swipeUp.direction = .up
+        self.tableView.addGestureRecognizer(swipeUp)
         tableView.isUserInteractionEnabled = true
         swipeUp.delegate = self
         swipeDown.delegate = self
@@ -155,34 +158,45 @@ class ListController: UIViewController {
     @objc func keyboardWillShow(notification: NSNotification) {
         let info:NSDictionary = notification.userInfo! as NSDictionary
         let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-
-        let keyboardHeight: CGFloat = keyboardSize.height
-
+        
+        var keyboardHeight: CGFloat = keyboardSize.height
+        if keyboard && lastKeyboardHeight != keyboardHeight {
+            keyboardHeight = lastKeyboardHeight
+        }
+        lastKeyboardHeight = keyboardHeight
         let _: CGFloat = info[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber as! CGFloat
-        self.createTextField(frame: CGRect(x: 0, y: (keyboardHeight) + 100, width: self.view.bounds.width, height: 60), leftButtons: [.addToList, .priority, .dueDate, .reminder, .favorite], rightButtons: [.done])
-
-        UIView.animate(withDuration: 0.25, delay: 0.25, options: .curveEaseInOut, animations: {
-        }, completion: nil)
+        print(lastKeyboardHeight)
+        //adding shifts it down
+        //+ 25 works for 11 390.0 44.0 896.0 = .435 +70
+        //iphone 11 pro max 390.0 44.0 896.0 = .435 +70
+        //iphone 11 pro in 380,44,812 = .467 -25  = +20
+        //-25 works for iphone8 304,44,667 .457 +20
+        //iphone 8 plus 315.0 44.0 736.0 = .42798 +70
+        //ipad pro 364.0 44.0 1024.0 = .35 +250
+        self.addTaskField.frame.origin.y = self.addTaskField.frame.origin.y - keyboardHeight - 44
     }
-
+    @objc func keyboardWillChangeFrame(notification: NSNotification) {
+        let info:NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        if keyboard == false {
+            keyboard = true
+            lastKeyboardHeight = keyboardSize.height
+        }
+    }
+    
     @objc func keyboardWillHide(notification: NSNotification) {
         let info: NSDictionary = notification.userInfo! as NSDictionary
         let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-
         let keyboardHeight: CGFloat = keyboardSize.height
         
         let _: CGFloat = info[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber as! CGFloat
-
-        UIView.animate(withDuration: 0.25, delay: 0.25, options: .curveEaseInOut, animations: {
-            self.addTaskField.removeFromSuperview()
-        }, completion: nil)
-            
-
+        self.addTaskField.frame.origin.y = self.view.frame.height
+ 
     }
     @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
-
+        
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-
+            
             switch swipeGesture.direction {
             case .down:
                 self.tableView.tableHeaderView?.isHidden = false
@@ -199,37 +213,37 @@ class ListController: UIViewController {
     }
     
     func configureNavBar() {
-//          titleLabel.font = UIFont(name: "OpenSans-Regular", size: 18)
-//          self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
-//          titleLabel.textColor = .blue
-//          navigationController?.navigationBar.barTintColor = .white
+        //          titleLabel.font = UIFont(name: "OpenSans-Regular", size: 18)
+        //          self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        //          titleLabel.textColor = .blue
+        //          navigationController?.navigationBar.barTintColor = .white
     }
 }
 //MARK: - UITextField
 extension ListController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-           switch textField {
-           case bigTextField:
-               listTitle = bigTextField.text ?? "Untitled List"
-               let list = ListObject()
-               list.name = listTitle
-               //need to update realmData, maybe send notification
-               list.position = (lists.count - 1) + 1
-               let results = uiRealm.objects(ListObject.self)
-               nameTaken = false
-               for result in results {
+        switch textField {
+        case bigTextField:
+            listTitle = bigTextField.text ?? "Untitled List"
+            let list = ListObject()
+            list.name = listTitle
+            //need to update realmData, maybe send notification
+            list.position = (lists.count - 1) + 1
+            let results = uiRealm.objects(ListObject.self)
+            nameTaken = false
+            for result in results {
                 if result.name == list.name {
-                  //we need to tell user that name is taken
-                  nameTaken = true
-                  let alertController = UIAlertController(title: "Name is already in use, please use a different name", message: "", preferredStyle: UIAlertController.Style.alert)
+                    //we need to tell user that name is taken
+                    nameTaken = true
+                    let alertController = UIAlertController(title: "Name is already in use, please use a different name", message: "", preferredStyle: UIAlertController.Style.alert)
                     let okayAction = UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: {
-                            (action : UIAlertAction!) -> Void in })
-
+                        (action : UIAlertAction!) -> Void in })
+                    
                     alertController.addAction(okayAction)
                     self.present(alertController, animated: true, completion: nil)
                 }
-               }
-               if !nameTaken {
+            }
+            if !nameTaken {
                 bigTextField.resignFirstResponder()
                 bigTextField.isUserInteractionEnabled = false
                 try! uiRealm.write {
@@ -238,21 +252,21 @@ extension ListController: UITextFieldDelegate {
                 reloadDelegate?.reloadTableView()
             }
             
-           default:
-               break
-           }
-           return true
-       }
-
-       func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-           return true
-       }
+        default:
+            break
+        }
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
 }
 
 //MARK: - TableView
 extension ListController: UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-            return true
+        return true
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         1
